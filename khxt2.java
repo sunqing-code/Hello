12a0a02f8bb46eeef782605764b324503a7217a5
sunqing-code <55099324+sunqing-code@users.noreply.github.com>
2019-10-22 22:12:31 +0800

    课后习题2

---
+++ khxt2.java
@@ -0,0 +1,17 @@
+
+public class khxt2 {
+	
+		
+		public static void main(String[] args){
+		new Thread (new MyRunnable()).start();
+		for (int i=0;i<100;i++){
+		System.out.println("main");
+		}
+		
+		}
+}
+class MyRunnable implements Runnable{
+		public void run(){
+		for(int i = 0;i<50;i++){
+		System.out.println("new");
+		}		}}

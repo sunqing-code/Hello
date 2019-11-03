400e17aaa041caee1d2d579afe52ff9d35035044
sunqing-code <55099324+sunqing-code@users.noreply.github.com>
2019-11-03 23:36:49 +0800

    课本例12

---
+++ Example12.java
@@ -0,0 +1,31 @@
+public class Example12 {
+
+	public static void main(String[] args) {
+		Ticket1 ticket=new Ticket1();
+		new Thread(ticket,"线程一").start();
+		new Thread(ticket,"线程二").start();
+		new Thread(ticket,"线程三").start();
+		new Thread(ticket,"线程四").start();
+	}
+
+}
+class Ticket1 implements Runnable{
+	private int tickets=10;
+	Object lock=new Object();
+	public void run(){
+		while(true){
+			synchronized(lock){
+				try{
+					Thread.sleep(10);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}
+				if(tickets>0){
+					System.out.println(Thread.currentThread().getName()+"---卖出的票"+tickets--);
+				}else{
+					break;
+				}
+			}
+		}
+	}
+}

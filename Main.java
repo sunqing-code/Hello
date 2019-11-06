e8cbfe7185977e568852f571aca450c68bc312f3
sunqing-code <55099324+sunqing-code@users.noreply.github.com>
2019-11-06 21:48:22 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
-import tv.*;
+import car.*;
 import factory.*;
 import utility.*;
-public class Main{
-	public static void main(String args[]) {
-		TV tv;
-		TVFactory factory;
-		factory=(TVFactory)XMLUtil.getBean();
-		tv=factory.produceTV();
-		tv.play();
-		}
+public class Main {
+
+	public static void main(String args[]){
+		Car ca;
+		Factory factory;
+	    factory = (Factory)XMLUtility.getBean();
+	    ca = factory.produceCar();
+	    TaxiBase tb=new TaxiBase();
+	    Input input=new Input(tb);
+	    Output output=new Output(tb);
+	    new Thread(input).start();
+	    new Thread(output).start();
+	 }
+
 }

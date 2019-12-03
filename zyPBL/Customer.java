71f721ef33e25a29a5f2b5f130b17eeccc3baf8a
sunqing-code <55099324+sunqing-code@users.noreply.github.com>
2019-12-03 19:13:49 +0800

    PBL第二次提交（switch单独写成一个方法）

--- zyPBL/Customer.java
+++ zyPBL/Customer.java
@@ -25,22 +25,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			
-			//determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-			        thisAmount += 2;
-			        if (each.getDaysRented() > 2)
-				       thisAmount += (each.getDaysRented() - 2) * 1.5;
-			        break;
-		        case Movie.NEW_RELEASE:
-			        thisAmount += each.getDaysRented() * 3;
-			        break;
-		        case Movie.CHILDRENS:
-			        thisAmount += 1.5;
-			        if (each.getDaysRented() > 3)
-				       thisAmount += (each.getDaysRented() - 3) * 1.5;
-			        break;
-             }
+			thisAmount = amountFor(each);
 			
             //add frequent renter points
 			frequentRenterPoints++;
@@ -60,7 +45,24 @@ public class Customer {
 	}
 
 
-    
+    private double amountFor (Rental each){
+    	double thisAmount = 0;
+    	switch (each.getMovie().getPriceCode()) {
+    	    case Movie.REGULAR:
+                 thisAmount += 2;
+	             if (each.getDaysRented() > 2)
+		            thisAmount += (each.getDaysRented() - 2) * 1.5;
+	             break;
+		    case Movie.NEW_RELEASE:
+			     thisAmount += each.getDaysRented() * 3;
+			     break;
+		    case Movie.CHILDRENS:
+			     thisAmount += 1.5;
+			     if (each.getDaysRented() > 3)
+				    thisAmount += (each.getDaysRented() - 3) * 1.5;
+			     break;
+             }
+    	return thisAmount;
    
-
+    }
 }

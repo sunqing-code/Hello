045238396cec186740954bda8d646ce706cfa010
sunqing-code <55099324+sunqing-code@users.noreply.github.com>
2019-12-03 18:42:21 +0800

    PBL第二次提交（switch单独写成一个方法）

--- first/Customer.java
+++ first/Customer.java
@@ -26,22 +26,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			
-           //determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
+			thisAmount = amountFor(each);  //第一次添加
 			
             //add frequent renter points
 			frequentRenterPoints++;
@@ -59,6 +44,24 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
 	}
+
+private double amountFor(Rental each){   //第一次修改
+	double thisAmount = 0;  //第一次添加
+    switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+    }
+    return thisAmount;   //
 }
-
-
+}

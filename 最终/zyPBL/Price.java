f71c8cb345748f30d2f02b7deb7b3cd53b353ff4
sunqing-code <55099324+sunqing-code@users.noreply.github.com>
2019-12-03 22:45:09 +0800

    PBL作业提交（最终）

---
+++ 最终/zyPBL/Price.java
@@ -0,0 +1,12 @@
+package zyPBL;
+
+abstract class Price {
+	abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented){
+		return 1;
+	}
+	
+
+}
